f81492d4b96e18785baaaa8df4d5710a2e11b09a
didier <b.didier745@laposte.net>
2020-03-23 11:54:29 +0100

    Add test QUnit

---
+++ front-end/tests/test_connection_servers.js
@@ -0,0 +1,23 @@
+QUnit.test("test QUnit", function( assert ) {
+	var url = "http://127.0.0.1:8080/"
+	assert.ok(true, "QUnit OK")
+})
+QUnit.test("connection server", function( assert ) {
+	var url = "http://127.0.0.1:8080/"
+	 $.ajax({
+		  type: "POST",
+		  url: url+"test",
+		  data: {},
+		  success: function(data, textStatus, jqXHR)
+		    {
+		        
+		    	assert.ok(true, "connection Ok")
+
+		    },
+		    error: function (jqXHR, textStatus, errorThrown)
+		    {
+				assert.ok(false, "connection failed")	 
+		    },
+		  	dataType: "json"
+		});
+})
