905671573d0f8925e289e671d3d4f7afaf802fcb
didier <b.didier745@laposte.net>
2020-03-26 10:24:25 +0100

    fic

--- front-end/tests/test_connection_servers.js
+++ front-end/tests/test_connection_servers.js
@@ -3,7 +3,9 @@ QUnit.test("test QUnit", function( assert ) {
 	assert.ok(true, "QUnit OK")
 })
 
-/*	var url = "http://54.36.98.109:8080/test"
+QUnit.test("test QUnit", function( assert ) {
+	var done = assert.async();
+	var url = "http://54.36.98.109:8080/test"
 
 	request.post(
 		url,
@@ -16,11 +18,15 @@ QUnit.test("test QUnit", function( assert ) {
 		if (error) {
 
 		  assert.ok(false,"connection server failed")
+		  done();
 		  return
 		}
 		else{
 			assert.ok(true, "connection server ok");
+			done();
 		}
 
 	});
+})
+/*	
 })*/
